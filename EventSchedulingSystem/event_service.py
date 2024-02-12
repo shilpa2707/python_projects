73ca7b917fd8deaebb6301971afbe8a10a44c07e
shilpa2707 <66522664+shilpa2707@users.noreply.github.com>
2024-02-11 22:37:41 -0600

    changes

---
+++ EventSchedulingSystem/event_service.py
@@ -0,0 +1,19 @@
+class EventService:
+
+    def __init__(self,title,date,time):
+        self.title = title
+        self.date = date
+        self.time = time
+
+    def add_event():
+        print()
+    
+    def fetch_event():
+        print()
+        
+    def update_event():
+        print()        
+    
+    def delete_record():
+        print()
+
